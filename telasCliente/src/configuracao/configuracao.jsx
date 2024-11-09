a264f96a3e7080eb6cb621daabb2186e92e70190
mxngs <gabueno43@gmail.com>
2024-11-09 16:08:43 -0300

    Configuração dando erro

--- telasCliente/src/configuracao/configuracao.jsx
+++ telasCliente/src/configuracao/configuracao.jsx
@@ -3,19 +3,17 @@ import Sidebar from '../components/Sidebar.jsx';
 import './App.css';
 
 const Configuracao = () => {
-    const [userData, setUserData] = useState(null); // Dados do usuário
-    const [isPremium, setIsPremium] = useState(false); // Status premium
-    const [currentView, setCurrentView] = useState('image'); // Controla qual painel mostrar
-    const [isEditing, setIsEditing] = useState(false); // Controla se estamos no modo de edição
-    const [formData, setFormData] = useState({}); // Dados do formulário para editar
+    const [userData, setUserData] = useState(null);
+    const [isPremium, setIsPremium] = useState(false);
+    const [currentView, setCurrentView] = useState('image');
+    const [isEditing, setIsEditing] = useState(false);
+    const [formData, setFormData] = useState({});
 
-    // Função para fazer logout
     const handleLogout = () => {
         localStorage.removeItem("id_cliente");
-        window.location.href = '/'; // Redireciona para a página de login ou inicial
+        window.location.href = '/';
     };
 
-    // Função para buscar os dados do cliente por ID
     const fetchUserData = async () => {
         const id = localStorage.getItem("id_cliente");
         if (!id) {
@@ -27,10 +25,9 @@ const Configuracao = () => {
             const response = await fetch(`https://touccan-backend-8a78.onrender.com/2.0/touccan/cliente/${id}`);
             if (response.ok) {
                 const data = await response.json();
-                console.log("Dados do cliente recebidos:", data);
                 setUserData(data.cliente);
-                setFormData(data.cliente); // Preenche os dados do formulário com os dados do cliente
-                setIsPremium(data.cliente.premium); // Atualiza o status premium
+                setFormData(data.cliente);
+                setIsPremium(data.cliente.premium);
             } else {
                 alert('Erro ao buscar dados do cliente');
             }
@@ -40,12 +37,10 @@ const Configuracao = () => {
         }
     };
 
-    // Função para alternar entre as views (painéis)
     const toggleView = (view) => {
         setCurrentView(view);
     };
 
-    // Função para alterar o status premium
     const togglePremium = async () => {
         const id = localStorage.getItem("id_cliente");
         if (!id) {
@@ -53,24 +48,18 @@ const Configuracao = () => {
             return;
         }
 
-        const newPremiumStatus = !isPremium; // Alterna entre premium e não premium
+        const newPremiumStatus = !isPremium;
         setIsPremium(newPremiumStatus);
 
-        // Verifique se precisa de autenticação, por exemplo, um token
-        const token = localStorage.getItem("auth_token"); // Se usar token, ou substitua conforme necessário
-        const headers = {
-            'Content-Type': 'application/json',
-        };
-
-        if (token) {
-            headers['Authorization'] = `Bearer ${token}`; // Supondo que a API use Bearer Token
-        }
+        const updatedFormData = { ...formData, premium: newPremiumStatus };
 
         try {
             const response = await fetch(`https://touccan-backend-8a78.onrender.com/2.0/touccan/cliente/${id}`, {
                 method: 'PUT',
-                headers: headers,
-                body: JSON.stringify({ premium: newPremiumStatus }), // Corrigido o formato aqui
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ cliente: updatedFormData }),
             });
 
             if (response.ok) {
@@ -78,23 +67,34 @@ const Configuracao = () => {
             } else {
                 const errorData = await response.json();
                 alert('Erro ao atualizar status Premium. Detalhes: ' + errorData.message);
-                setIsPremium(!newPremiumStatus); // Reverte o estado caso a requisição falhe
+                setIsPremium(!newPremiumStatus);
             }
         } catch (error) {
             console.error('Erro ao atualizar o status Premium:', error);
             alert('Erro na atualização. Tente novamente.');
-            setIsPremium(!newPremiumStatus); // Reverte o estado em caso de erro
+            setIsPremium(!newPremiumStatus);
         }
     };
 
-    // Função para lidar com a edição do formulário
+    const validateFormData = () => {
+        const requiredFields = ['nome_fantasia', 'telefone', 'email', 'cep'];
+        for (let field of requiredFields) {
+            if (!formData[field]) {
+                alert(`O campo ${field} é obrigatório.`);
+                return false;
+            }
+        }
+        return true;
+    };
+
     const handleFormChange = (e) => {
         const { name, value } = e.target;
         setFormData({ ...formData, [name]: value });
     };
 
-    // Função para salvar as alterações
     const handleSaveChanges = async () => {
+        if (!validateFormData()) return;
+
         const id = localStorage.getItem("id_cliente");
         if (!id) {
             alert('ID do cliente não encontrado');
@@ -107,17 +107,17 @@ const Configuracao = () => {
                 headers: {
                     'Content-Type': 'application/json',
                 },
-                body: JSON.stringify({ cliente: formData }), // Corrigido para enviar dados do cliente
+                body: JSON.stringify({ cliente: formData }),
             });
 
             if (response.ok) {
-                const updatedData = await response.json(); // Verifica a resposta da API
-                console.log("Dados atualizados:", updatedData);
+                const updatedData = await response.json();
                 alert('Dados atualizados com sucesso!');
-                setUserData(updatedData.cliente); // Atualiza os dados na interface
-                setIsEditing(false); // Fecha o modo de edição
+                setUserData(updatedData.cliente);
+                setIsEditing(false);
             } else {
-                alert('Erro ao salvar alterações.');
+                const errorData = await response.json();
+                alert('Erro ao salvar alterações. Detalhes: ' + errorData.message);
             }
         } catch (error) {
             console.error('Erro ao salvar as alterações:', error);
@@ -125,7 +125,6 @@ const Configuracao = () => {
         }
     };
 
-    // Carrega os dados do cliente ao montar o componente
     useEffect(() => {
         fetchUserData();
     }, []);
@@ -133,10 +132,9 @@ const Configuracao = () => {
     return (
         <div className="container">
             <Sidebar toggleView={toggleView} />
-
+            
             <div className="cards-panel">
                 <div className="card" onClick={() => toggleView('account')}>
-                    <div className="card-detail"></div>
                     <div className="card-content">
                         <i className="fas fa-info-circle card-icon"></i>
                         <div>
@@ -146,7 +144,6 @@ const Configuracao = () => {
                     </div>
                 </div>
                 <div className="card" onClick={() => toggleView('security')}>
-                    <div className="card-detail"></div>
                     <div className="card-content">
                         <i className="fas fa-lock card-icon"></i>
                         <div>
@@ -156,7 +153,6 @@ const Configuracao = () => {
                     </div>
                 </div>
                 <div className="card" onClick={() => toggleView('support')}>
-                    <div className="card-detail"></div>
                     <div className="card-content">
                         <i className="fas fa-headset card-icon"></i>
                         <div>
@@ -166,7 +162,6 @@ const Configuracao = () => {
                     </div>
                 </div>
                 <div className="card" onClick={() => toggleView('aboutUs')}>
-                    <div className="card-detail"></div>
                     <div className="card-content">
                         <img src="./logo - preto.png" alt="Sobre Nós" className="card-icon" />
                         <div>
@@ -176,7 +171,6 @@ const Configuracao = () => {
                     </div>
                 </div>
                 <div className="card" onClick={() => toggleView('premium')}>
-                    <div className="card-detail"></div>
                     <div className="card-content">
                         <img src="./icons8-coroa-30 4.png" alt="Premium" className="card-icon" />
                         <div>
@@ -189,13 +183,11 @@ const Configuracao = () => {
 
             <div className="separator"></div>
 
-            {/* Painel de Imagem Inicial */}
             <div className="image-panel" style={{ display: currentView === 'image' ? 'flex' : 'none' }}>
                 <img src="./logo - preto.png" alt="Descrição da Imagem" className="logo-image" />
                 <div className="image-description">O que deseja acessar?</div>
             </div>
 
-            {/* Painel de Informações da Conta */}
             {currentView === 'account' && userData && (
                 <div className="account-details">
                     <h2 className="account-title">Informações da Conta</h2>
@@ -246,24 +238,77 @@ const Configuracao = () => {
                                 <div className="info"><strong>Nome do Usuário:</strong> {userData.nome_fantasia}</div>
                                 <div className="info"><strong>Telefone:</strong> {userData.telefone}</div>
                                 <div className="info"><strong>E-mail:</strong> {userData.email}</div>
-                                <button onClick={() => setIsEditing(true)}>Editar</button>
+                                <div className="info"><strong>CEP:</strong> {userData.cep}</div>
+                                <button onClick={() => setIsEditing(true)}>Editar Informações</button>
                             </div>
                         )}
                     </div>
-                    <div className="logout-button">
-                        <button onClick={handleLogout}>Sair</button>
+                    <button onClick={handleLogout}>Sair</button>
+                </div>
+            )}
+
+            {currentView === 'premium' && (
+                <div className="premium-details">
+                    <h2 className="account-title">Serviço Premium</h2>
+                    <div className="premium-content">
+                        <p>O serviço premium permite acesso exclusivo a novos recursos.</p>
+                        <button onClick={togglePremium}>
+                            {isPremium ? 'Cancelar Premium' : 'Tornar-se Premium'}
+                        </button>
                     </div>
                 </div>
             )}
 
-            {/* Painel de Premium */}
-            {currentView === 'premium' && (
-                <div className="premium-section">
-                    <h2>Status Premium</h2>
-                    <p>Você está {isPremium ? 'Premium' : 'não Premium'}</p>
-                    <button onClick={togglePremium}>
-                        {isPremium ? 'Cancelar Premium' : 'Tornar-se Premium'}
-                    </button>
+            {currentView === 'security' && (
+                <div className="security-settings">
+                    <h2 className="account-title">Segurança</h2>
+                    <div className="account-card">
+                        <div className="security-form">
+                            <div className="info">
+                                <div className="info-title">Senha Atual:</div>
+                                <input type="password" placeholder="Escreva sua senha atual" />
+                            </div>
+                            <div className="info">
+                                <div className="info-title">Nova Senha:</div>
+                                <input type="password" placeholder="Pelo menos 8 caracteres" />
+                            </div>
+                            <div className="info">
+                                <div className="info-title">Confirmar Senha:</div>
+                                <input type="password" placeholder="Pelo menos 8 caracteres" />
+                            </div>
+                            <button className="logout-button">Atualizar Senha</button>
+                        </div>
+                    </div>
+                </div>
+            )}
+
+            {currentView === 'support' && (
+                <div className="support-panel">
+                    <h2 className="account-title">Suporte</h2>
+                    <div className="account-card">
+                        <div className="contato">E-mail para contato</div>
+                        <div className="info-value">contato.touccan@gmail.com</div>
+                        <div className="info-title">Encontrou algum problema? reporte para nós</div>
+                        <textarea id="support-message"  rows="4" placeholder="Digite sua mensagem aqui..."></textarea>
+                        <div className="character-count">0/500 caracteres</div>
+                        <button className="send-button">Enviar Reporte</button>
+                    </div>
+                </div>
+            )}
+
+            {currentView === 'aboutUs' && (
+                <div className="about-us">
+                    <h2 className="account-title">Sobre Nós</h2>
+                    <div className="account-card">
+                        <div className="sub-title">Quem Somos</div>
+                        <div className="info-value">
+                            Touccan é uma forma inovadora de conectar pessoas que precisam de empregos temporários de alta remuneração a alguém que precisa de serviços de curto prazo e eficientes. Nossa plataforma permite que as pessoas realizem suas tarefas e contrate-as, com foco naqueles que precisam apenas por um curto período.
+                        </div>
+                        <div className="sub-title">Nossa Missão</div>
+                        <div className="info-value">
+                            Nossa missão é simplificar o processo de emprego e contratação, tornando-o ágil e acessível. Acreditamos que todos devem ter a oportunidade de encontrar trabalho que se ajuste às suas necessidades e que os empregadores merecem acesso rápido a profissionais qualificados para serviços temporários.
+                        </div>
+                    </div>
                 </div>
             )}
         </div>
